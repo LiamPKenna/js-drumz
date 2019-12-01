39b0b4f277c55fede683a8386a0f5519ee4fb112
LiamPKenna <liampkenna@gmail.com>
2019-12-01 06:07:23 -0800

    clear template

--- src/main.js
+++ src/main.js
@@ -3,26 +3,15 @@ import $ from 'jquery';
 import './css/bootstrap.min.css';
 import './css/styles.css';
 import {Howl} from 'howler';
-import {DrumKit} from './js/drumKit.js';
+
 
 // MAIN LOGIC
-// import { Class } from './js/class.js';
 
-const drumKit = new DrumKit(1);
 
-const kick = new Howl({
-  src: [drumKit.kick]
-});
 
 // USER INTERFACE
 $(document).ready(function(){
-  $('#noAge').click(() => {
-    kick.play();
-  });
-  $('#up').click(() => {
-    kick.rate(kick.rate() + .5);
-  });
-  $('#down').click(() => {
-    kick.rate(kick.rate() - .5);
-  });
+
+
+
 });
