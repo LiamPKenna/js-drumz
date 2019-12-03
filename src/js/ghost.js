c2bafd02e08bbc29e45ae8b6f69424a2f91ab624
LiamPKenna <liampkenna@gmail.com>
2019-12-03 15:59:59 -0800

    add ghost synth object

---
+++ src/js/ghost.js
@@ -0,0 +1,27 @@
+import Pizzicato from 'pizzicato';
+import notes from './notes.js';
+
+export class Ghost {
+  constructor() {
+    this.sound = new Pizzicato.Sound({
+      source: 'wave',
+      options: {
+        volume: 0.79,
+        frequency: 440,
+        attack: 0.01
+      }
+    });
+    this.dub = new Pizzicato.Effects.DubDelay({
+      feedback: 0.4,
+      time: 0.25,
+      mix: 0.4,
+      cutoff: 600
+    });
+    this.sound.addEffect(this.dub);
+    this.notes = notes;
+  }
+
+  changeNote(n) {
+    this.sound.frequency = this.notes[n];
+  }
+}
