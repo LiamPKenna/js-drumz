741c1651dc12712f821ec3eb8bf07aff573d4580
LiamPKenna <liampkenna@gmail.com>
2019-12-03 11:01:25 -0800

    add handling for kit selection

--- src/main.js
+++ src/main.js
@@ -66,6 +66,11 @@ $(document).ready(function(){
     sequencer.changePitch(track, pitch);
   });
 
+  $('#dropdownMenuButton').change(() => {
+    const kitSelected = $('#dropdownMenuButton option:selected').val();
+    sequencer.selectKit(kitSelected);
+  });
+
   refreshSequence();
 
 });
